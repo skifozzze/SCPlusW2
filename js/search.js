919811cc752307539e49ad9a1ed73731f66c8c30
skifozzze <skifozzze@gmail.com>
2022-09-30 17:49:47 +0300

    hwW5

--- js/search.js
+++ js/search.js
@@ -3,7 +3,27 @@ const citySearch = function (event) {
   let city = document.querySelector(".text");
   let cityNow = document.querySelector(".city");
   cityNow.innerHTML = `${city.value}`;
+
+  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=ac209dae1f283fb332a5bb7f50b0f468`;
+
+  const showTemperature = function (response) {
+    let tempNow = Math.round(response.data.main.temp);
+    
+    let temp = document.querySelector(".temperature");
+    temp.innerHTML = `${tempNow}`;
+  };
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 };
 
-let citySearchForm = document.querySelector(".city-search_form");
+let citySearchForm = document.querySelector(".city-search_form ");
 citySearchForm.addEventListener("submit", citySearch);
+
+
+
+
+
+
+
+
